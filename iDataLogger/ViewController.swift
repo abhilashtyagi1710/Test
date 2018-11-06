c03fe89d54eb5e835ddc00c179f05109138d6aac
Abhilash Tyagi <abhilash.tyagi1710@gmail.com>
2018-11-05 18:00:21 -0800

    Initial import

---
+++ iDataLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDataLogger
+//
+//  Created by Abhilash Tyagi on 11/5/18.
+//  Copyright © 2018 Abhilash Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
